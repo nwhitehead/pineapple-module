7b45dbf4cf217f8701ed311188f62d5958f23a37
Nathan Whitehead <nwhitehe@gmail.com>
2015-08-30 12:28:25 -0700

    First version.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='pineapple',
+  version='0.1',
+  description='Utility functions for the Pineapple IPython/Jupyter front-end',
+  url='https://github.com/nwhitehead/pineapple-module/',
+  author='Nathan Whitehead',
+  author_email='nwhitehe@gmail.com',
+  license='MIT',
+  packages=['pineapple'],
+  zip_safe=False
+)
+
